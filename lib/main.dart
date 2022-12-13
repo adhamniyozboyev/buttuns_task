3737804807e189e95995364b272837ed531f178e
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2022-12-13 09:20:22 +0500

    gallery start

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Mytask(),
+  ));
+}
+
+class Mytask extends StatefulWidget {
+  const Mytask({Key? key}) : super(key: key);
+  @override
+  State<Mytask> createState() => _MytaskState();
+}
+
+class _MytaskState extends State<Mytask> {
+  int index = 0;
+  List path = [
+    'image/windows2.jpg',
+    'image/windows3.jpg',
+    'image/windows4.jpg',
+    'image/windows5.jpg'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Container(
+          height: 700,
+          child: Image.asset(path[index%4],fit: BoxFit.contain,),
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+                onPressed: () {
+                  if(index>0)
+                  setState(() {
+                    index--;
+                  });
+                },
+                child: Icon(Icons.arrow_back_ios)),
+                 Text('Image :$index'),
+        TextButton(onPressed: (){
+          if(index<3)
+          setState(() {
+            index++;
+          });
+        }, child: Icon(Icons.arrow_forward_ios)),
+          ],
+        ),
+       
+
+      ]),
+    );
+  }
+}
